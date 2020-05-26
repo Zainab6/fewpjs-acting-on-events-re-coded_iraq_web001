49e663ec589052b72cd3758ee9fb8bfc47756123
Zainab Saud <zai99nb@gmail.com>
2020-05-26 19:19:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 let dodger = document.getElementById("dodger");
 document.addEventListener("keydown", moveDodgerLeft(e))
 function moveDodgerLeft(e) {
-  if( e.key === "ArrowLeft"){
+  if ( e.key === "ArrowLeft") {
     let leftNumbers = dodger.style.left.replace("px","");
-    let left = parseInt(leftNumbers,10);
+    let left = parseInt(leftNumbers, 10);
     if (left > 0){
       dodger.style.left = `${left-1}px`;
     }
